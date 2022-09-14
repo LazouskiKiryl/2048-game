461f056306a83ef094766564fc36eacde6a73002
LazouskiKiryl <kiryllazouski@gmail.com>
2022-09-14 15:48:54 +0300

    feat: add styles for Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { TilesType } from '../game/Game';
 import TileComponent from './TileComponent';
 
-const WIDTH = '300px';
-
 const StyledBoard = styled.div`
+  box-sizing: border-box;
   position: relative;
-  width: ${WIDTH};
-  height: ${WIDTH};
+  width: 100%;
   margin: 0 auto;
+  padding: 15px;
   background: #bbada0;
+  border-radius: 6px;
 `;
 
 type BoardProps = {
@@ -18,8 +18,29 @@ type BoardProps = {
 };
 
 const Board: React.FC<BoardProps> = ({ tiles }) => {
+  const boardRef = useRef<HTMLDivElement>(null);
+
+  const resize = () => {
+    if (boardRef.current) {
+      const width = getComputedStyle(boardRef.current).getPropertyValue('width');
+      boardRef.current.style.height = width;
+    }
+  };
+
+  const handleResize = useCallback(resize, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [handleResize]);
+
+  useEffect(resize, []);
+
   return (
-    <StyledBoard>
+    <StyledBoard ref={boardRef}>
       {tiles.map((row, rowIndex) =>
         row.map(
           (tile, colIndex) =>
